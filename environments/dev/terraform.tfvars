ff62754afca6f23aee19cc52ea7d5c16742dc780
mtegalapalli <mtegalapalli@gmail.com>
2025-07-21 10:11:35 +0000

    Update project IDs and buckets

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="ansible-463603"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "ansible-463603-tfstate"
     prefix = "env/prod"
   }
 }
